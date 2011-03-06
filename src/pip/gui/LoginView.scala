7bf1310cab568cb058ce671718ad49ece1d4695b
Jiri Prajzner <jiri@mb-pro.local>
2011-03-06 23:43:43 +0100

    standardni dialog s moznosti vstupu. chybi reakce na akce a upravy vhzledu a chovani

--- src/pip/gui/LoginView.scala
+++ src/pip/gui/LoginView.scala
@@ -2,22 +2,17 @@ package pip.gui
 import scala.swing._
 
 object LoginView extends SimpleSwingApplication {
-  val parent = new FlowPanel {
-    contents += new Button("OK")
-    contents += new Label("Uzivatel")
-  }
+  val parent = new TextField(10)
 
   def top = new MainFrame {
     contents = new FlowPanel {
       contents += new Button {
         action = Action("click") {
           Dialog.showInput(parent,
-                 message = "Please login",
-                 title = "Login",
+                 message = "Uzivatelske jmeno",
+                 title = "Prihlaseni",
                  messageType = Dialog.Message.Plain,
-                 optionType = Dialog.Options.OkCancel,
-                 entries = Seq("Login", "Cancel"),
-                 initial = 1)
+                 initial = "")
         }
       }
    }
