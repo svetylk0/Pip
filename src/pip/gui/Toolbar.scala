bfe1176822529d157ea38be7b0a7d9228f1efa58
Antonin Brettsnajdr <svetylk0@seznam.cz>
2011-03-14 15:12:09 +0100

    uprava lokalizace + mentions

--- src/pip/gui/Toolbar.scala
+++ src/pip/gui/Toolbar.scala
@@ -4,9 +4,8 @@ import pip.core.Loc
 import scala.swing._
 
 object Toolbar extends BoxPanel(Orientation.Horizontal) {
-      
       contents += new Button(Loc("home"))
       contents += new Button(Loc("profile"))
       contents += new Button(Loc("messages"))
-      contents += new Button(Loc("who to follow"))
+      contents += new Button(Loc("whoToFollow"))
 }
