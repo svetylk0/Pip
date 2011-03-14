d627195c69db0e809459e31258be358daa043152
unknown <jiri@.(none)>
2011-03-14 12:21:12 +0100

    pridana lokalizace

--- src/pip/gui/Toolbar.scala
+++ src/pip/gui/Toolbar.scala
@@ -4,8 +4,8 @@ import scala.swing._
 
 object Toolbar extends BoxPanel(Orientation.Horizontal) {
       
-      contents += new Button("Home")
-      contents += new Button("Profile")
-      contents += new Button("Messages")
-      contents += new Button("Who to follow")
+      contents += new Button(Loc("home"))
+      contents += new Button(Loc("profile"))
+      contents += new Button(Loc("messages"))
+      contents += new Button(Loc("who to follow"))
 }
