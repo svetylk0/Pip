104f0dbaf8139cb36d653f439db4fd8c0db7ea64
Jiri Prajzner <jiri@mb-pro.local>
2011-03-14 08:35:18 +0100

    plne funkcni tabbed pane se scroll pane

--- src/pip/gui/Tabs.scala
+++ src/pip/gui/Tabs.scala
@@ -7,11 +7,14 @@ object Tabs extends SimpleSwingApplication {
   val b  = new Button("OK") 
   val tp1 = new TabbedPane.Page("First tab", ta1)
   val tp2 = new TabbedPane.Page("Second tab", b)
+  val tabs = new TabbedPane {
+    pages += tp1
+    pages += tp2
+  }
+  val sp = new ScrollPane(tabs)
+
   def top = new MainFrame {
     title = "Tabs demo"
-    contents = new TabbedPane {
-      pages += tp1
-      pages += tp2
-    }
+    contents = sp 
   }
 }
